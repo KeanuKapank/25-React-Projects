38572783b8bfed213338132ec1a0f8ee6bf99425
Keanu Kapank <keanuk@betsoftware.com>
2025-07-11 17:41:24 +0200

    Created Image Slider App

--- image-slider/src/App.tsx
+++ image-slider/src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import ImageSlider from "./components/ImageSlider";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        limit={10}
+      ></ImageSlider>
+    </>
+  );
 }
 
 export default App;
