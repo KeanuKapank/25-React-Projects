bf70e6406054c249d1ffd2866e15f544b7d7f958
Keanu Kapank <keanuk@betsoftware.com>
2025-07-17 15:43:52 +0200

    Tabs Setup

---
+++ tabs/src/components/Tabs.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./style.css";
+
+const Tabs = ({ tabsContent, onChange }) => {
+  const [currentTabIndex, setCurrentTabIndex] = useState(0);
+
+  function handleOnClick(getCurrentIndex: number) {
+    setCurrentTabIndex(getCurrentIndex);
+    onChange(getCurrentIndex);
+  }
+
+  return (
+    <div className="wrapper">
+      <div className="heading">
+        {tabsContent.map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleOnClick(index)}
+            className={`tab-item ${
+              currentTabIndex === index ? "active" : "inactive"
+            }`}
+          >
+            {item.label}
+          </div>
+        ))}
+      </div>
+      <div className="content">
+        {tabsContent[currentTabIndex] && tabsContent[currentTabIndex].content}
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
